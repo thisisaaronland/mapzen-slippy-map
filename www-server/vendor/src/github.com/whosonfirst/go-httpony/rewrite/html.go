cb512c716a91fd2be91281f0428178fa2a6f7c47
thisisaaroland <aaron@mapzen.com>
2016-06-27 17:59:52 -0700

    use go-httpony for basic rewrite stuff

---
+++ www-server/vendor/src/github.com/whosonfirst/go-httpony/rewrite/html.go
@@ -0,0 +1,44 @@
+package rewrite
+
+import (
+	"golang.org/x/net/html"
+	"io"
+	"net/http"
+)
+
+type HTMLRewriter interface {
+	Rewrite(node *html.Node, writer io.Writer) error
+	SetKey(key string, value interface{}) error
+}
+
+type HTMLRewriteHandler struct {
+	writer HTMLRewriter
+}
+
+func NewHTMLRewriterHandler(writer HTMLRewriter) (*HTMLRewriteHandler, error) {
+
+	h := HTMLRewriteHandler{
+		writer: writer,
+	}
+
+	return &h, nil
+}
+
+func (h HTMLRewriteHandler) Handler(reader io.Reader) http.Handler {
+
+	fn := func(rsp http.ResponseWriter, req *http.Request) {
+
+		doc, err := html.Parse(reader)
+
+		if err != nil {
+			http.Error(rsp, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		h.writer.SetKey("request", req)
+		h.writer.Rewrite(doc, rsp)
+		return
+	}
+
+	return http.HandlerFunc(fn)
+}
