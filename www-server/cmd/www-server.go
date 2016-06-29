6faf761f2369c7d34aec6b454d425733efac65f5
thisisaaroland <aaron@mapzen.com>
2016-06-29 09:46:38 -0700

    start using httpony/sso - assume nothing works yet

--- www-server/cmd/www-server.go
+++ www-server/cmd/www-server.go
@@ -1,89 +1,18 @@
 package main
 
 import (
-	"crypto/md5"
-	"encoding/hex"
 	"flag"
 	"fmt"
 	"github.com/thisisaaronland/go-slippy-tiles"
 	slippy "github.com/thisisaaronland/go-slippy-tiles/provider"
-	"github.com/vaughan0/go-ini"
 	"github.com/whosonfirst/go-httpony/cors"
-	"github.com/whosonfirst/go-httpony/crypto"
-	"github.com/whosonfirst/go-httpony/rewrite"
-	"github.com/whosonfirst/go-httpony/tls"
-	"golang.org/x/net/html"
-	"golang.org/x/oauth2"
-	"io"
+	"github.com/whosonfirst/go-httpony/sso"
+	"github.com/whosonfirst/go-httpony/tls"	
 	"net/http"
 	"os"
 	"path/filepath"
 	"regexp"
 )
-
-func NewTestRewriter() (*TestRewriter, error) {
-	t := TestRewriter{}
-	return &t, nil
-}
-
-type TestRewriter struct {
-	rewrite.HTMLRewriter
-	Request *http.Request
-	Secret  string
-}
-
-func (t *TestRewriter) SetKey(key string, value interface{}) error {
-
-	if key == "request" {
-		req := value.(*http.Request)
-		t.Request = req
-	}
-
-	if key == "secret" {
-		t.Secret = value.(string)
-	}
-
-	return nil
-}
-
-func (t *TestRewriter) Rewrite(node *html.Node, writer io.Writer) error {
-
-	var f func(node *html.Node, writer io.Writer)
-
-	f = func(n *html.Node, w io.Writer) {
-
-		if n.Type == html.ElementNode && n.Data == "body" {
-
-			t_cookie, _ := t.Request.Cookie("t")
-
-			crypt, _ := crypto.NewCrypt(t.Secret)
-			token, _ := crypt.Decrypt(t_cookie.Value)
-
-			token_ns := ""
-			token_key := "data-api-access-token"
-			token_value := token
-
-			token_attr := html.Attribute{token_ns, token_key, token_value}
-			n.Attr = append(n.Attr, token_attr)
-
-			endpoint_ns := ""
-			endpoint_key := "data-api-endpoint"
-			endpoint_value := "fix-me"
-
-			endpoint_attr := html.Attribute{endpoint_ns, endpoint_key, endpoint_value}
-			n.Attr = append(n.Attr, endpoint_attr)
-		}
-
-		for c := n.FirstChild; c != nil; c = c.NextSibling {
-			f(c, w)
-		}
-	}
-
-	f(node, writer)
-
-	html.Render(writer, node)
-	return nil
-}
 
 func main() {
 
@@ -97,7 +26,6 @@ func main() {
 	var tls_key = flag.String("tls-key", "", "Path to an existing TLS key. If absent a self-signed key will be generated.")
 	var proxy_tiles = flag.Bool("proxy", false, "Proxy and cache tiles locally.")
 	var proxy_config = flag.String("proxy-config", "", "Path to a valid config file for slippy tiles.")
-	var rewrite_html = flag.Bool("rewrite-html", false, "...")
 
 	var sso_enable = flag.Bool("sso", false, "...")
 	var sso_config = flag.String("sso-config", "", "...")
@@ -118,24 +46,14 @@ func main() {
 	handler := cors.EnsureCORSHandler(fs, *cors_enable, *cors_allow)
 
 	var re_tile *regexp.Regexp
-	var re_html *regexp.Regexp
 
-	var re_signin *regexp.Regexp
-	var re_auth *regexp.Regexp
+	var tiles_provider slippytiles.Provider
 
-	var provider slippytiles.Provider
-
-	var writer *TestRewriter
-	var rewriter *rewrite.HTMLRewriteHandler
-
-	var sso_cfg ini.File
+	var sso_provider *sso.SSOProvider
 
 	if *sso_enable {
 
-		re_signin, _ = regexp.Compile(`/signin/?$`)
-		re_auth, _ = regexp.Compile(`/auth/?$`)
-
-		sso_cfg, err = ini.LoadFile(*sso_config)
+		sso_provider, err = sso.NewSSOProvider(*sso_config)
 
 		if err != nil {
 			panic(err)
@@ -152,7 +70,7 @@ func main() {
 			panic(err)
 		}
 
-		provider, err = slippy.NewProviderFromConfig(config)
+		tiles_provider, err = slippy.NewProviderFromConfig(config)
 
 		if err != nil {
 			panic(err)
@@ -161,133 +79,23 @@ func main() {
 		re_tile, _ = regexp.Compile(`/(.*)/(\d+)/(\d+)/(\d+).(\w+)$`)
 	}
 
-	if *rewrite_html {
-
-		writer, _ = NewTestRewriter()
-		rewriter, _ = rewrite.NewHTMLRewriterHandler(writer)
-
-		re_html, _ = regexp.Compile(`/(?:(?:.*).html)?$`)
-	}
-
 	juggler := func(rsp http.ResponseWriter, req *http.Request) {
 
 		url := req.URL
 		path := url.Path
 
-		// https://godoc.org/golang.org/x/oauth2#example-Config
-
+		// this is not quite right but I haven't wrapped my head around
+		// cascading handlers yet... (201606029/thisisaaronland)
+		
 		if *sso_enable {
 
-			// please check all of this sooner
-
-			oauth_client, _ := sso_cfg.Get("oauth", "client_id")
-			oauth_secret, _ := sso_cfg.Get("oauth", "client_secret")
-			oauth_auth_url, _ := sso_cfg.Get("oauth", "auth_url")
-			oauth_token_url, _ := sso_cfg.Get("oauth", "token_url")
-
-			// shrink to 32 characters
-
-			hash := md5.New()
-			hash.Write([]byte(oauth_secret))
-			crypto_secret := hex.EncodeToString(hash.Sum(nil))
-
-			crypto, err := crypto.NewCrypt(crypto_secret)
-
-			if err != nil {
-				http.Error(rsp, err.Error(), http.StatusInternalServerError)
-				return
-			}
-
-			writer.SetKey("secret", crypto_secret)
-
-			scheme := "http"
-
-			if *tls_enable {
-				scheme = "https"
-			}
-
-			redirect_url := fmt.Sprintf("%s://%s/auth/", scheme, endpoint)
-
-			conf := &oauth2.Config{
-				ClientID:     oauth_client,
-				ClientSecret: oauth_secret,
-				Scopes:       []string{},
-				Endpoint: oauth2.Endpoint{
-					AuthURL:  oauth_auth_url,
-					TokenURL: oauth_token_url,
-				},
-				RedirectURL: redirect_url,
-			}
-
-			if re_signin.MatchString(path) {
-				url := conf.AuthCodeURL("state", oauth2.AccessTypeOnline)
-				http.Redirect(rsp, req, url, 302)
-				return
-			}
-
-			if re_auth.MatchString(path) {
-
-				query := req.URL.Query()
-				code := query.Get("code")
-
-				if code == "" {
-					http.Error(rsp, "Missing code parameter", http.StatusBadRequest)
-					return
-				}
-
-				token, err := conf.Exchange(oauth2.NoContext, code)
-
-				if err != nil {
-					http.Error(rsp, err.Error(), http.StatusBadRequest)
-					return
-				}
-
-				t, err := crypto.Encrypt(token.AccessToken)
-
-				if err != nil {
-					http.Error(rsp, err.Error(), http.StatusInternalServerError)
-					return
-				}
-
-				t_cookie := http.Cookie{Name: "t", Value: t, Expires: token.Expiry, Path: "/", HttpOnly: true, Secure: *tls_enable}
-				http.SetCookie(rsp, &t_cookie)
-
-				http.Redirect(rsp, req, "/", 302)
-				return
-			}
-
+		   handler := sso_provider.Handler(docroot, *tls_enable)
+		   handler.ServeHTTP(rsp, req)
+		   return		   
 		}
 
 		if *proxy_tiles && re_tile.MatchString(path) {
-			handler := provider.Handler()
-			handler.ServeHTTP(rsp, req)
-			return
-		}
-
-		if *rewrite_html && re_html.MatchString(path) {
-
-			abs_path := filepath.Join(docroot, path)
-
-			info, err := os.Stat(abs_path)
-
-			if err != nil {
-				http.Error(rsp, err.Error(), http.StatusInternalServerError)
-				return
-			}
-
-			if info.IsDir() {
-				abs_path = filepath.Join(abs_path, "index.html")
-			}
-
-			reader, err := os.Open(abs_path)
-
-			if err != nil {
-				http.Error(rsp, err.Error(), http.StatusInternalServerError)
-				return
-			}
-
-			handler := rewriter.Handler(reader)
-
+			handler := tiles_provider.Handler()
 			handler.ServeHTTP(rsp, req)
 			return
 		}
---
+++ www-server/vendor/src/github.com/whosonfirst/go-httpony/sso/sso.go
@@ -0,0 +1,239 @@
+package sso
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"errors"
+	"github.com/vaughan0/go-ini"
+	"github.com/whosonfirst/go-httpony/crypto"
+	"github.com/whosonfirst/go-httpony/rewrite"
+	"golang.org/x/net/html"
+	"golang.org/x/oauth2"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"regexp"
+)
+
+func NewSSORewriter(crypt *crypto.Crypt) (*SSORewriter, error) {
+	t := SSORewriter{Crypto: crypt}
+	return &t, nil
+}
+
+type SSORewriter struct {
+	rewrite.HTMLRewriter
+	Request *http.Request
+	Crypto   *crypto.Crypt
+}
+
+func (t *SSORewriter) SetKey(key string, value interface{}) error {
+
+	if key == "request" {
+		req := value.(*http.Request)
+		t.Request = req
+	}
+
+	return nil
+}
+
+func (t *SSORewriter) Rewrite(node *html.Node, writer io.Writer) error {
+
+	var f func(node *html.Node, writer io.Writer)
+
+	f = func(n *html.Node, w io.Writer) {
+
+		if n.Type == html.ElementNode && n.Data == "body" {
+
+			t_cookie, _ := t.Request.Cookie("t")
+			token, _ := t.Crypto.Decrypt(t_cookie.Value)
+
+			token_ns := ""
+			token_key := "data-api-access-token"
+			token_value := token
+
+			token_attr := html.Attribute{token_ns, token_key, token_value}
+			n.Attr = append(n.Attr, token_attr)
+
+			endpoint_ns := ""
+			endpoint_key := "data-api-endpoint"
+			endpoint_value := "fix-me" // FIX ME
+
+			endpoint_attr := html.Attribute{endpoint_ns, endpoint_key, endpoint_value}
+			n.Attr = append(n.Attr, endpoint_attr)
+		}
+
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			f(c, w)
+		}
+	}
+
+	f(node, writer)
+
+	html.Render(writer, node)
+	return nil
+}
+
+type SSOProvider struct {
+	Crypto  *crypto.Crypt
+	Writer *SSORewriter
+	OAuth  *oauth2.Config
+}
+
+func NewSSOProvider(sso_config string) (*SSOProvider, error) {
+
+	sso_cfg, err := ini.LoadFile(sso_config)
+
+	if err != nil {
+		return nil, err
+	}
+
+	oauth_client, ok := sso_cfg.Get("oauth", "client_id")
+
+	if !ok {
+		return nil, errors.New("Invalid client_id")
+	}
+
+	oauth_secret, ok := sso_cfg.Get("oauth", "client_secret")
+
+	if !ok {
+		return nil, errors.New("Invalid client_secret")
+	}
+
+	oauth_auth_url, ok := sso_cfg.Get("oauth", "auth_url")
+
+	if !ok {
+		return nil, errors.New("Invalid auth_url")
+	}
+
+	oauth_token_url, ok := sso_cfg.Get("oauth", "token_url")
+
+	if !ok {
+		return nil, errors.New("Invalid token_url")
+	}
+
+	// oauth_api_url, ok := sso_cfg.Get("oauth", "api_url")
+	
+	// shrink to 32 characters
+
+	hash := md5.New()
+	hash.Write([]byte(oauth_secret))
+	crypto_secret := hex.EncodeToString(hash.Sum(nil))
+
+	crypt, err := crypto.NewCrypt(crypto_secret)
+
+	if err != nil {
+		return nil, err
+	}
+
+	writer, err := NewSSORewriter(crypt)
+
+	if err != nil {
+		return nil, err
+	}
+
+	redirect_url := "fix me" // FIXME
+
+	conf := &oauth2.Config{
+		ClientID:     oauth_client,
+		ClientSecret: oauth_secret,
+		Scopes:       []string{},
+		Endpoint: oauth2.Endpoint{
+			AuthURL:  oauth_auth_url,
+			TokenURL: oauth_token_url,
+		},
+		RedirectURL: redirect_url,
+	}
+
+	pr := SSOProvider{
+		Crypto:  crypt,
+		Writer: writer,
+		OAuth:  conf,
+	}
+
+	return &pr, nil
+}
+
+func (s *SSOProvider) Handler(docroot string, tls_enable bool) http.HandlerFunc { // FIXME - put tls_enable somewhere better...
+
+	re_signin, _ := regexp.Compile(`/signin/?$`)
+	re_auth, _ := regexp.Compile(`/auth/?$`)
+	re_html, _ := regexp.Compile(`/(?:(?:.*).html)?$`)
+
+	rewriter, _ := rewrite.NewHTMLRewriterHandler(s.Writer)
+
+	f := func(rsp http.ResponseWriter, req *http.Request) {
+
+		url := req.URL
+		path := url.Path
+
+		if re_signin.MatchString(path) {
+			url := s.OAuth.AuthCodeURL("state", oauth2.AccessTypeOnline)
+			http.Redirect(rsp, req, url, 302)
+			return
+		}
+
+		if re_auth.MatchString(path) {
+
+			query := req.URL.Query()
+			code := query.Get("code")
+
+			if code == "" {
+				http.Error(rsp, "Missing code parameter", http.StatusBadRequest)
+				return
+			}
+
+			token, err := s.OAuth.Exchange(oauth2.NoContext, code)
+
+			if err != nil {
+				http.Error(rsp, err.Error(), http.StatusBadRequest)
+				return
+			}
+
+			t, err := s.Crypto.Encrypt(token.AccessToken)
+
+			if err != nil {
+				http.Error(rsp, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			t_cookie := http.Cookie{Name: "t", Value: t, Expires: token.Expiry, Path: "/", HttpOnly: true, Secure: tls_enable}
+			http.SetCookie(rsp, &t_cookie)
+
+			http.Redirect(rsp, req, "/", 302) // FIXME - do not simply redirect to /
+			return
+		}
+
+		if re_html.MatchString(path) {
+
+			abs_path := filepath.Join(docroot, path)
+
+			info, err := os.Stat(abs_path)
+
+			if err != nil {
+				http.Error(rsp, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			if info.IsDir() {
+				abs_path = filepath.Join(abs_path, "index.html")
+			}
+
+			reader, err := os.Open(abs_path)
+
+			if err != nil {
+				http.Error(rsp, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			handler := rewriter.Handler(reader)
+
+			handler.ServeHTTP(rsp, req)
+			return
+		}
+
+		// FIXME - what about all the other files?
+	}
+
+	return http.HandlerFunc(f)
+}
